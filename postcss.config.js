2723365303c5501cd9ad83eb95e4b83104fe0011
yoshiro <76180677+YoshiroMaximus@users.noreply.github.com>
2024-10-01 13:40:32 -0700

    redo

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,svelte,ts,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
